1e2ee741ece51ad6c1b9138400093aeee0b16740
AdryanaP <dryportuga@gmail.com>
2022-02-26 19:23:23 -0300

    feat: component card estilizado

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,32 +1,54 @@
-import styles from './Cards.module.css'
+import Link from "next/link";
+import { useState, useEffect } from "react";
 
-export default function () {
+export default function (props) {
+
+
   const cards = [
     {
-      title: "Titulo",
-      description: "lorem ajdijdiajd",
+      title: "Linux",
+      description: "10 questões",
     },
     {
-      title: "Titulo",
-      description: "lorem ajdijdiajd",
+      title: "DevOps",
+      description: "10 questões",
     },
     {
-      title: "Titulo",
-      description: "lorem ajdijdiajd",
+      title: "Docker",
+      description: "10 questões",
     },
     {
-      title: "Titulo",
-      description: "lorem ajdijdiajd",
+      title: "Programming ",
+      description: "10 questões",
     },
   ];
   const listCards = cards.map((card) => {
     return (
-      <div className={styles.cardContent}>
-        <p>{card.title}</p>
-        <p>{card.description}</p>
+      <div
+        className="p-8 md:p-12 bg-white rounded text-center w-64 md:w-80 space-y-8 my-8"
+        key={card.title}
+      >
+        <div className="space-y-1">
+          <p className="text-xl font-bold highlight">{card.title}</p>
+          <p>{card.description}</p>
+        </div>
+        <button className="bg-gray-300 p-2 rounded">
+          <Link
+            href={{
+              pathname: "/question",
+              query: { name: props.name },
+            }}
+          >
+            Responder
+          </Link>
+        </button>
       </div>
     );
   });
 
-  return <section className={styles.card}>{listCards}</section>;
+  return (
+    <section className="grid md:grid-cols-2 justify-center items-center justify-items-center md:gap-8 p-8 lg:px-20 md:mt-32 fullscreen">
+      {listCards}
+    </section>
+  );
 }
